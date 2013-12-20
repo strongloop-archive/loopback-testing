6dbfb945f569cc58862bd3907a850e64fae998e1
Ritchie Martori <skawful@gmail.com>
2013-12-20 14:15:06 -0800

    Factor out LoopBack dep to removing coupling on the exact loopback instance.

--- test/test.js
+++ test/test.js
@@ -23,8 +23,7 @@ describe('helpers', function () {
   describe('helpers.describe', function() {
     ['staticMethod',
      'whenLoggedInAsUser',
-     'whenCalledAnonymously',
-     'model']
+     'whenCalledAnonymously']
     .forEach(function(func) {
       it('should have a method named ' + func, function () {
         assert.equal(typeof helpers.describe[func], 'function');
@@ -44,10 +43,10 @@ describe('helpers', function () {
   });
 
   describe('helpers.beforeEach.givenModel', function() {
-    helpers.beforeEach.givenModel('AccessToken');
-    it('should have an AccessToken property', function () {
-      assert(this.AccessToken);
-      assert(this.AccessToken.id);
+    helpers.beforeEach.givenModel('xxx-test-model');
+    it('should have an xxx-test-model property', function () {
+      assert(this['xxx-test-model']);
+      assert(this['xxx-test-model'].id);
     });
   });
 

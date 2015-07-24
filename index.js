d9f3c02e7021dbf11e43996b5c57ca9c185088e7
Lemon Cola <lemoncola.apps@gmail.com>
2015-07-24 11:37:00 +0800

    Add util.[dumpHttpContext, dumpHttpRequest, dumpHttpResponse]

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var helpers = require('./lib/helpers');
 exports.describe = helpers.describe;
 exports.it = helpers.it;
 exports.beforeEach = helpers.beforeEach;
+exports.util = helpers.util;
 exports.TestDataBuilder = require('./lib/test-data-builder');

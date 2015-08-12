0719b31a2a2c3166945f3278f390d1948f9d9a5a
Are Almaas <mralmaas@gmail.com>
2015-08-12 20:19:38 +0200

    add helper method for notfound when unauthenticated

--- test/test.js
+++ test/test.js
@@ -18,6 +18,7 @@ describe('helpers', function () {
      'shouldBeDeniedWhenCalledAnonymously',
      'shouldBeAllowedWhenCalledUnauthenticated',
      'shouldBeDeniedWhenCalledUnauthenticated',
+     'shouldBeNotFoundWhenCalledUnauthenticated',
      'shouldBeAllowedWhenCalledByUser',
      'shouldBeValidCreateResponse',
      'shouldBeValidGetAllResponse',
